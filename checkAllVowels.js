4bb3ed433732914251f46de26b48e01fb88d8514
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-03-06 11:56:43 +0700

    function-check-all-vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function checkAllVowel(str) {
+    str = str.toLowerCase();
+    var vowels = 'aiueo';
+    var countVowels = 0;
 
+    for (var i = 0; i < vowels.length; i++) {
+        for (var j = 0; j < str.length; j++) {
+            if (vowels[i] === str[j]) {
+                countVowels++;
+            }
+        }
+    }
+
+    return str.length === countVowels;
+}
+
+console.log(checkAllVowel('bc'));
+console.log(checkAllVowel('bac'));
+console.log(checkAllVowel('aaaiiiuua'));
