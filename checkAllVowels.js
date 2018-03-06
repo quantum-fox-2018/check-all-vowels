f70175f6c0733d76790c0fff670e76f24429e2b0
Taufik Hidayat <ovick_kd@yahoo.com>
2018-03-06 11:55:19 +0700

    check vocals

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkVocals(arr) {
 
+  let vocals = 'aiueo'
+
+  for (var i = 0; i < arr.length; i++) {
+
+    if (vocals.indexOf(arr[i]) == -1) {
+      return false
+    }
+
+  }
+
+  return true
+
+}
+
+console.log(checkVocals('aiueo'));
+console.log(checkVocals('aiueb'));
+console.log(checkVocals('aaaaauuuuuaoiue'));
+console.log(checkVocals('bjkso'));
