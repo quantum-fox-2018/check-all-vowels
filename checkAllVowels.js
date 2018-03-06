3806841397b22518a1472ef72986942805516792
Zena <zena.vilenia@gmail.com>
2018-03-06 10:43:38 +0700

    Tugas Check All Vowels Zena Vilenia

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+function checkAllVowels(str) {
+  let vokal = ["A", "I", "U", "E", "O"];
+  str = str.toUpperCase().split('');
 
+  for(let i in str) {
+    if(vokal.indexOf(str[i]) === -1)
+      return false;
+  }
+
+  return true;
+}
+
+console.log(checkAllVowels("sdawwda"));
+console.log(checkAllVowels("AjdiwVksd"));
+console.log(checkAllVowels("Aiu"));
