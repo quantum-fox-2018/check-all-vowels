03981f7fd7529f28630ae4c60c8219a3c28c651b
Natascha Lamsu <tazcha15@gmail.com>
2018-03-06 22:04:08 +0700

    check all vowels clear

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,16 @@
+function checkVowels(str) {
+  var vocals = ['a', 'i', 'u', 'e', 'o'];
+  str = str.split('');
 
+  for(var i = 0; i < vocals.length; i++) {
+    for(var j = 0; j < str.length; j++) {
+      if(vocals.indexOf(str[j]) === -1) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log(checkVowels('abrakadabra'));
+console.log(checkVowels('uuaaaieiioo'));
