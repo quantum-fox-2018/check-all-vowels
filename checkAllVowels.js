bb98c9e95ce1acd15534f0859054257180934713
JoviAndrew <jovian_andrew@hotmail.com>
2018-03-06 12:56:09 +0700

    update code

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -4,24 +4,12 @@
 function checkVowel(str){
     let vowels = ['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'];
 
-    //Normal Way
-    //=============
-    // for(let counter=0; counter<str.length; counter++){
-    //     for(let counter2 = 0; counter2<vowels.length; counter2++){
-    //         if(str.charAt(counter) === vowels[counter2]) return true;
-    //     }
-    // }
-
-    // return false;
-
-    //Recursive
-    //=============
-    if(str.length === 0) return false;
+    if(str.length === 0) return true;
     else{
         for(let counter = 0; counter<vowels.length; counter++){
-            if(str.charAt(0) === vowels[counter]) return true;
+            if(str.charAt(0) === vowels[counter]) return checkVowel(str.slice(1));
         }
-        return checkVowel(str.slice(1));
+        return false;
     }
 }
 
@@ -29,3 +17,4 @@ console.log(checkVowel('Bob'));
 console.log(checkVowel('Blob'));
 console.log(checkVowel('Bb'));
 console.log(checkVowel('bbbbbbbbbbbbbbbbbbbbbbaBb'));
+console.log(checkVowel('aaaaaaaaaaaaaaaaaaaaa'));
