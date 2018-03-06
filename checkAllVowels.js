1241c951f85e90a29f293bf870cf3b22c942147f
faizal <faizalramanda@gmail.com>
2018-03-06 11:40:09 +0700

    check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,22 @@
+var vocal = ['a', 'i', 'u', 'e', 'o']
 
+function checkVocal(str) {
+  var jumlah = 0
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < vocal.length; j++) {
+      if (str[i] === vocal[j]) {
+        jumlah += 1
+      }
+    }
+  }
+  if (jumlah === 5) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(checkVocal('aaaaa'));
+console.log(checkVocal('aiueo'));
+console.log(checkVocal('abbaa'));
