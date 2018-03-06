dad5ec18d88497ec3d1057bfa2731b0b40567dcb
ekifakhrureza <ekifakhrureza@gmail.com>
2018-03-06 16:10:33 +0700

    VOWEL CHECK

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkAllVowels(input){
+  let vowel ='aiueo'
+  let counter=0
+  for (var i = 0; i < input.length; i++) {
+    if(vowel.indexOf(input[i])===-1){
+      counter++
+    }
+  }
+  if(counter>0){
+    return false
+  }
+  else {
+    return true
+  }
+}
 
+console.log(checkAllVowels('qwertyuiop'));
+console.log(checkAllVowels('aoiueoiue'));
+console.log(checkAllVowels('ooiiuu'));
+console.log(checkAllVowels('ppjdsds'));
