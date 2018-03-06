433bbd80630efb21ac7f5ae2adddb77fbc085be2
fastabiqul1993 <fastabiqul1993@gmail.com>
2018-03-06 11:49:58 +0700

    AdhyFastabiqul

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,31 @@
+function checkVowel (sentence) {
 
+    var vocal = ['a','i','u','e','o']
+    var temp = 0
+
+    for (var i = 0; i < sentence.length; i++) {
+        
+        for (var j = 0; j < vocal.length; j++) {
+            
+            if (sentence[i] === vocal[j]) {
+
+                temp ++
+            }
+
+        }
+    }
+
+    if (temp === sentence.length) {
+
+        return true
+    } else {
+
+        return false
+    }
+
+}
+
+console.log(checkVowel('aaiiuu'))
+console.log(checkVowel('aiiiuuuuuuaaaeeee'))
+console.log(checkVowel('abcd'))
+console.log(checkVowel('bcd'))
