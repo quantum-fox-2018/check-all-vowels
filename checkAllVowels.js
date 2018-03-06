ea4d7bae478a6a3330ea4b98cd0bb9c220b124b4
richo andy <richoandy@gmail.com>
2018-03-06 11:06:19 +0700

    challenge done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+const vowels = ["a", "i", "u", "e", "o"]
 
+function checkAllVowels(sentence){
+  var flag = true;
+  for (var i = 0; i < sentence.length; i++) {
+    if (vowels.indexOf(sentence[i]) === -1) {
+      flag = false;
+    }
+  }
+
+  if (flag) {
+    return true;
+  }else{
+    return false;
+  }
+}
+
+console.log(checkAllVowels("aaaaa")) //true
+console.log(checkAllVowels("aiueo")) //true
+console.log(checkAllVowels("abcde")) //false
+console.log(checkAllVowels("aaaab")) //false
