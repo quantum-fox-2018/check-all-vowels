f59f74149bdf8a7854b3f6de84f91ca1bceb6c10
Dani Damara <danny12march@gmail.com>
2018-03-06 11:24:41 +0700

    Tugas Cek Vowels Dani Damara

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,22 @@
+//Check All Vowals
+function checkAllVowels(str){
+    let vocals = 'aiueo';
 
+    if(str.length < 1){
+        return true;
+    }else{
+        let sliceStr = str.slice(1);
+        for(let i = 0; i < vocals.length; i++){
+            if(str[0] === vocals[i]){
+                return true == checkAllVowels(sliceStr);
+            }
+        }
+        return false;
+    }
+
+}
+
+
+console.log(checkAllVowels('cobaa')); //false
+console.log(checkAllVowels('auiuiooouoiuoe')); //true
+console.log(checkAllVowels('eaiiiaaoow')); //false
