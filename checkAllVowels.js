5c8707dfab2d5c2f0e59f38f9cb1ac182df193b2
NeilTanado <neiltanado@yahoo.co.id>
2018-03-06 15:40:18 +0700

    first push

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+function cekVocal(str) {
+  var alpabet = 'aiueo';
+  var jumlah = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (alpabet.includes(str[i])) {
+      jumlah ++;
+    }
+  }
+  if (jumlah < str.length) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
+
+console.log(cekVocal('aaaa')); // true
+console.log(cekVocal('babab')); // false
+console.log(cekVocal('bbbb')); // false
+console.log(cekVocal('aoius')); // false
+console.log(cekVocal('auao')); // true
