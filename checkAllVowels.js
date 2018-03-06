bafab912f9feaafa048b781b01a4d47676b166e9
davidjoshua87 <davidjosh_87@yahoo.com>
2018-03-06 13:48:23 +0700

    checkAllVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,27 @@
+// function checkAllVowels(str) {
+//   for (var i = 0; i < str.length; i++) {
+//     if (str[i] !== 'a' && str[i] !== 'i' && str[i] !== 'u' && str[i] !== 'e' && str[i] !== 'o') {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
+function checkAllVowels(str) {
+  var hitung = 0;
+  var newVowel = 'aiueo';
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < newVowel.length; j++) {
+      if (str[i] === newVowel[j]) {
+        hitung++;
+      }
+    }
+  }
+  return (hitung === str.length);
+}
+
+console.log(checkAllVowels('aikkku')); // false
+console.log(checkAllVowels('aaaaa')); // true
+console.log(checkAllVowels('aaiueo')); // true
+console.log(checkAllVowels('abcaeo')); // false
+console.log(checkAllVowels('aiueooeui')); // true
