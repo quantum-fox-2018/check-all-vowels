220f1180ffb9693360b85c050afe44e4a66cb4a8
barestu <fheartnet@gmail.com>
2018-03-06 12:43:59 +0700

    Menambahkan cara rekursif

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,9 +1,10 @@
 function checkAllVowels (str) {
   var hurufVokal = 'aiueoAIUEO';
 
+  /*
   // CARA BIASA
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < hurufVokal.length; j++) {
+  for (let i in str) {
+    for (let j in hurufVokal) {
       if (str.charAt(i) === hurufVokal.charAt(j)) {
         return true;
       }
@@ -11,6 +12,19 @@ function checkAllVowels (str) {
   }
 
   return false;
+  */
+
+  // CARA RECURSIVE
+  if (str.length === 0) {
+    return false;
+  } else {
+    for (let i in hurufVokal) {
+      if (str.charAt(0) === hurufVokal.charAt(i)) {
+        return true;
+      }
+    }
+    return checkAllVowels(str.slice(1));
+  }
 }
 
 console.log(checkAllVowels('yeaa')); // true
