bd3623d86791e54e18fe16c804ca012e0e69ea95
Muhammad Ramadiansyah <muhammadramadiansyah@Muhammads-MacBook-Pro.local>
2018-03-06 12:01:56 +0700

    tugas cari huruf vokal rama

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkAllVowels(str){
+  var vowels = ['a','e','i','o','u'];
+  var condition = true;
 
+  if(str.length>0){
+    for(var i=0;i<vowels.length;i++){
+      if(str[0] == vowels[i]){
+        return checkAllVowels(str.slice(1));
+      }
+    }
+      return false;
+    }
+  else{
+    return true;
+  }
+}
+
+console.log(checkAllVowels('w'));
+console.log(checkAllVowels('eaaa'));
+console.log(checkAllVowels('eaiueoaa'));
