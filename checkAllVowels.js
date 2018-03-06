42b49e71bb8f87cccb683b5061a10679e94172dd
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-06 11:18:25 +0700

    done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function checkAllVowels(input){
+  let vow = 'aiueo';
+  let count = 0;
+  for(let i=0; i<input.length; i++){
+    for(let j=0; j<vow.length; j++){
+      if(input[i].toLowerCase()===vow[j]){
+        count++;
+      }
+    }
+  }
+  if(count == input.length){
+    return true;
+  }
+  return false;
+}
 
+console.log(checkAllVowels('aiueo'));
+console.log(checkAllVowels('ai6eo'));
+console.log(checkAllVowels('aIueo'));
