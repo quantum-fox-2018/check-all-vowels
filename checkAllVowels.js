fd576a8ed126a74c787655541568bdfab553c9b4
OkyWiliarso <okywiliarso@gmail.com>
2018-03-06 12:04:16 +0700

    add checkAllVowels.js

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
 
+function checkVocal(string){
+
+  var vokal = 'aiueo';
+  for(var i = 0; i < string.length; i++){
+      if (vokal.indexOf(string[i]) !== -1){
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+}
+
+console.log(checkVocal('aiueo'));
+console.log(checkVocal('eoaiou'));
+console.log(checkVocal('mouse'));
+console.log(checkVocal('vokal'));
