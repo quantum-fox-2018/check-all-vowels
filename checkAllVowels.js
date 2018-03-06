2a4767f0e9af10e43c36457315be6f455f184992
yasirmaulana <yasir.maulana@gmail.com>
2018-03-06 18:55:00 +0700

    day 2, check-all-vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+function checkAllVowels(string){
+  var arrVokal = ['a','i','u','e','o']
+  var count = 0
+  for(var i=0; i<string.length; i++){
+    for(var j=0; j<arrVokal.length; j++){
+      if(string[i]===arrVokal[j]){
+        count++
+      }
+    }
+  }
+  // console.log(count)
+  if(count===string.length){
+    return true
+  }
+  else {
+    return false
+  }
+}
 
+console.log(checkAllVowels('rei')) //false
+console.log(checkAllVowels('ueeeiao')) //true
