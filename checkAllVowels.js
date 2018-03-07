3116ba56cdfa17b228e2a4505d6a831011de2751
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-03-07 10:38:58 +0700

    validasiKapital

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,5 +1,5 @@
 function cekVokal(str){
-  var vokal='aiueo'
+  var vokal='aiueoAIUEO'
   var count=[]
   for (var i = 0; i < str.length; i++) {
     for (var j = 0; j < vokal.length; j++) {
@@ -20,5 +20,6 @@ function cekVokal(str){
 console.log(cekVokal('amkan'));//false
 console.log(cekVokal('aiueo'));//true
 console.log(cekVokal('abbbe'));//false
-console.log(cekVokal('abbae'))//FALSE
-console.log(cekVokal('aabae'))//FALSE
+console.log(cekVokal('abbae'));//FALSE
+console.log(cekVokal('aabae'));//FALSE
+console.log(cekVokal('AIUEO'));//true
