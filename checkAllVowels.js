57d5b5fa6407c5eb575043ef1aeb3aebc7ce0377
OkyWiliarso <okywiliarso@gmail.com>
2018-03-07 16:44:02 +0700

    check all vowels revision - okywiliarso

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -3,7 +3,7 @@ function checkVocal(string){
 
   var vokal = 'aiueo';
   for(var i = 0; i < string.length; i++){
-      if (vokal.indexOf(string[i]) !== -1){
+      if (vokal.indexOf(string[i].toLowerCase()) !== -1){
         return true;
       } else {
         return false;
@@ -16,3 +16,5 @@ console.log(checkVocal('aiueo'));
 console.log(checkVocal('eoaiou'));
 console.log(checkVocal('mouse'));
 console.log(checkVocal('vokal'));
+console.log(checkVocal('AAA'));
+console.log(checkVocal('OIOI'));
